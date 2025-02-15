b4e2ff0eadb0468e9a4e13fe41fd3c67ad06b2ce
hthomas222 <154530730+hthomas222@users.noreply.github.com>
2025-02-15 15:19:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 
 app = Flask(__name__)
@@ -13,12 +15,47 @@ def index():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('test'))
+        un = request.form['username']
+        pw = request.form['password']
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        for i in rows:
+            if un == i[1]: 
+                if pw == i[2]:
+                    return redirect(url_for('test'))
+                else:
+                    error = 'Invalid Credentials. Please try again.'
+            else: 
+                error = 'Invalid Credentials. Please try again.'
+        conn.close()
+        return render_template("auth.html", error=error)
     return render_template("auth.html", error=error)
 
+
+@app.route('/create-user', methods=['GET', 'POST'])
+def create_user():
+    return render_template("register.html")
+
+
+@app.route('/create-user/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        un = request.form['user']
+        pw = request.form['pw']
+        if len(pw) >= 12:
+            con = sqlite3.connect("users.db")
+            cur = con.cursor()
+            cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (un,pw))
+            con.commit()
+            con.close()
+            return render_template("auth.html", error=error)
+        else: 
+            error="Password must be longer than 12!"
+            return render_template("register.html", error=error)
+    return render_template("register.html", error=error)
 
 @app.route('/home')
 def test():
@@ -48,3 +85,5 @@ def logout():
 
 if __name__ == '__main__':
      app.run(port=8080, debug=True)
+
+
